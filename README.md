098620edf2f87bd7aa082c9dd15a0b66dd595888
lucasbrustolin <lucasbrustolin@hotmail.com>
2020-08-11 01:26:36 -0300

    TicTacToe

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Jogo-da-velha-com-React
-Bora criar um web app de #️⃣ jogo da velha, utilizando ⚛️ React &amp; ⚛ React Hooks! 💚
+Bora criar um web app de #?? jogo da velha, utilizando ?? React &amp; ? React Hooks! ??
+
+
+
+![TicTacToe.gif](./src/TicTacToe.gif)
+
+
+### `yarn start`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+The page will reload if you make edits.<br />
+You will also see any lint errors in the console.
+
+
+#Refer�ncia
+
+####Lucas Caton 
+https://www.youtube.com/watch?v=kFXDcaUcOto&feature=youtu.be
---
+++ src/TicTacToe.jsx
@@ -0,0 +1,105 @@
+import React, {  useState, useEffect } from 'react';
+import './TicTacToe.css';
+
+function TicTacToe() {
+  const emptyBoard = Array(9).fill("");
+  const [board, setBoard] = useState(emptyBoard);
+ 
+  const [currentPlayer, SetcurrentPlayer] = useState("O");
+  const [winner, setWinner] = useState(null);
+
+  const handleCellClick = (index) => {
+    if (winner) {
+      console.log("Jogo finalizado");
+      return null;
+    }      
+
+    if (board[index] !== "") {
+        console.log("Posição ocupada");
+        return null;
+    }  
+    
+    setBoard( 
+        board.map((item, itemIndex)  => itemIndex === index ? currentPlayer : item )
+      );
+
+      SetcurrentPlayer(currentPlayer === "X"  ? "O" : "X");
+  }
+
+
+  const checkWinner = () => {
+    const possibleWaysToWin = [
+      [board[0], board[1], board[2]],
+      [board[3], board[4], board[5]],
+      [board[6], board[7], board[8]],
+
+      [board[0], board[3], board[6]],
+      [board[1], board[4], board[7]],
+      [board[2], board[5], board[8]],
+      
+      [board[0], board[4], board[8]],
+      [board[2], board[4], board[6]],
+    ];
+
+    possibleWaysToWin.forEach(cells => {
+      if (cells.every(cell => cell ===  "O")) setWinner("O");
+      if (cells.every(cell => cell ===  "X")) setWinner("X");
+    });
+ 
+    checkDraw();
+
+  }
+
+  //-- Empatou
+  const checkDraw = () =>  {
+      if (board.every(item => item !== "") )  setWinner("E");
+    }
+    
+  
+  useEffect( checkWinner,[board]);
+    
+
+  const resetGame = () => {
+    SetcurrentPlayer("O");
+    setBoard(emptyBoard);
+    setWinner(null);
+  }
+
+  return (
+
+    <main>
+      <h1 className="title"> Jogo da velha </h1>
+    
+      <div className={`board $(winner ? "game-over" : ""}`}>
+        {board.map((item, index) => (
+          <div 
+            key={index}
+            className={`cell  ${item}`}
+            onClick={(() => handleCellClick(index))}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+  
+      {winner &&
+        <footer>
+          {winner === "E" ? 
+            <h2 className="winner-message">
+              <span className={winner}> Empatou!</span>
+            </h2>
+          :
+            <h2 className="winner-message">
+              <span className={winner}>{winner}</span> venceu ! 
+            </h2>       
+          }
+
+          <button onClick={resetGame}>Recomecar jogo!</button>
+        </footer>
+      }
+    </main>
+  
+  );
+}
+
+export default TicTacToe;
